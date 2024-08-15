07ebb209b519444845518560f14f1e4388a16729
Gabriel <64184571+GabrielOniz@users.noreply.github.com>
2024-08-15 20:40:14 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,33 +1,6 @@
-from flask import Flask, request, make_response, redirect, abort;
-app = Flask(__name__);
-
+# A very simple Flask Hello World app for you to get started with...
+from flask import Flask
+app = Flask(__name__)
 @app.route('/')
-def index():
-    return '<h1>Hello World!</h1>';
-
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name);
-
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent');
-    return '<p>Your browser is {}</p>'.format(user_agent);
-
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return '<p>Bad request</p>', 400;
-
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>This document carries a cookie!</h1>');
-    response.set_cookie('answer', '42');
-    return response
-
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/');
-
-@app.route('/abortar')
-def abortar():
-    abort(404);
+def hello_world():
+    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
