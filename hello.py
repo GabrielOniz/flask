017b0b0c470cd910282e39a3ca88c535e5a74937
Gabriel <64184571+GabrielOniz@users.noreply.github.com>
2026-08-10 23:37:45 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,10 +4,10 @@ app = Flask(__name__);
 @app.route('/')
 def index():
     
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto = '<h1>Avaliação contínua: Aula 040</h1>';    
     html_texto += '<ul>';    
     html_texto += '<li><a href="/">Home</a>';
-    html_texto += '<li><a href="/user/Fabio Teixeira/PT23820X/IFSP">Identificação</a>';
+    html_texto += '<li><a href="/user/Gabriel Oniz/PT3019781/IFSP">Identificação</a>';
     html_texto += '<li><a href="/contextorequisicao">Contexto da requisição</a>';
     html_texto += '</ul>';
     
@@ -15,7 +15,7 @@ def index():
 
 @app.route('/user/<name>/<prontuario>/<institution>')
 def user(name, prontuario, institution):
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto = '<h1>Avaliação contínua: Aula 040</h1>';    
     html_texto += '<h2>Aluno: {}</h2>'.format(name);
     html_texto += '<h2>Prontuário: {}</h2>'.format(prontuario);
     html_texto += '<h2>Instituição: {}</h2>'.format(institution);
@@ -26,7 +26,7 @@ def user(name, prontuario, institution):
 @app.route('/contextorequisicao')
 def contextorequisicao():
     user_agent = request.headers.get('User-Agent');
-    html_texto = '<h1>Avaliação contínua: Aula 030</h1>';    
+    html_texto = '<h1>Avaliação contínua: Aula 040</h1>';    
     html_texto += '<h2>Seu navegador é: {}</h2>'.format(user_agent);
     html_texto += '<h2>O IP do cumputador remoto é: {}</h2>'.format(request.remote_addr);
     html_texto += '<h2>O host da aplicação é: {}</h2>'.format(request.host);
